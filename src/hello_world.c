366757729b62a60ae323420a3a4434bb926589e5
Prashanth Rajaram <prashanthr@users.noreply.github.com>
2015-11-18 23:20:28 -0600

    Added real time to display

--- src/hello_world.c
+++ src/hello_world.c
@@ -2,6 +2,23 @@
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
+
+  // Write the current hours and minutes into a buffer
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
+                                          "%H:%M" : "%I:%M", tick_time);
+
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, s_buffer);
+}
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
+
 static void main_window_load(Window *window) {
   // Get information about the Window
   Layer *window_layer = window_get_root_layer(window);
@@ -21,7 +38,6 @@ static void main_window_load(Window *window) {
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 }
-
 static void main_window_unload(Window *window) {
   // Destroy TextLayer
   text_layer_destroy(s_time_layer);
@@ -36,11 +52,15 @@ static void init() {
     .load = main_window_load,
     .unload = main_window_unload
   });
+  
+  // Register with TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
+  // Make sure the time is displayed from the start
+  update_time();
 }
-
 static void deinit() {
   // Destroy Window
   window_destroy(s_main_window);
